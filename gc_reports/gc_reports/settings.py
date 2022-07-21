83419f42c5e9ecc802c30126996ce7ac3e1b2dc7
BarabanovPS <barabanov.ps@grandcasino.by>
2022-07-21 16:10:20 +0300

    Fixed: TimeZone

--- gc_reports/gc_reports/settings.py
+++ gc_reports/gc_reports/settings.py
@@ -109,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = '+3'
+TIME_ZONE = 'Europe/Minsk'
 
 USE_I18N = True
 
