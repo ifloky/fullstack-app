676f8a8f3b14c377c675a86387c08c6aa7924283
BarabanovPS <barabanov.ps@grandcasino.by>
2023-02-08 10:12:00 +0300

    Fixed: access

--- load_json_to_db.py
+++ load_json_to_db.py
@@ -1,9 +1,11 @@
 import asyncio
+import time
 import json
 import os
 import psycopg2
 
 from mysite import credentials
+from datetime import datetime, timedelta
 
 
 async def load_json_data(file_name):
@@ -20,7 +22,7 @@ async def save_data_to_db(file_name):
     cur = conn.cursor()
     for data in json_data:
         cur.execute(
-            'INSERT INTO public.rounds2 (round_id, account_id, game_id, created_at) VALUES (%s, %s, %s, %s)',
+            'INSERT INTO public.rounds (round_id, account_id, game_id, created_at) VALUES (%s, %s, %s, %s)',
             (data['round_id'], data['account_id'], data['game_id'], data['created_at']))
     conn.commit()
     cur.close()
@@ -28,10 +30,23 @@ async def save_data_to_db(file_name):
 
 
 async def main():
-    dir_path = 'N93_20230202T160053'
+    current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    start_job_time = time.perf_counter()
+    print(f"Start script at {current_date}")
+    dir_path = 'N93_20230206T165940'
     file_list = os.listdir(dir_path)
     tasks = [asyncio.create_task(save_data_to_db(f'{dir_path}/{file_name}')) for file_name in file_list]
     await asyncio.gather(*tasks)
 
+    stop_job_time = time.perf_counter()
+    working_time = stop_job_time - start_job_time
 
-asyncio.run(main())
+    print(f"Script finished at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+    print("Working time:", str(timedelta(seconds=working_time)))
+    print(f'Время выполнения: {time.perf_counter() - start_job_time:0.4f} seconds')
+    from memory_profiler import memory_usage
+    print("Затрачено памяти:", (memory_usage())[-1], "Mb")
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
