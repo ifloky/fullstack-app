271910e73464118aca1ff1cec8873fa267336023
BarabanovPS <barabanov.ps@grandcasino.by>
2023-03-31 12:26:38 +0300

    Fixed: Host not available, error stop scripts

--- check_rounds.py
+++ check_rounds.py
@@ -69,6 +69,7 @@ async def get_round_data_from_skks(skks_host, transaction_id):
             amount = response.json()['amount']
             return cmd, amount
     except (ConnectionError, ConnectTimeoutError) as e:
+        print(e)
         await asyncio.sleep(5)
         response = requests.post(
             url=skks_host,
